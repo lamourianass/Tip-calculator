26ae32dd570461149d6ac8dbc49957dfe0dfedf3
Anass Lamouri <anasslamouri@icloud.com>
2025-08-06 16:52:17 +0100

    Done the calculateTotal function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const btnEl = document.getElementById('calculate')
+const billInput = document.getElementById('bill')
+const tipInput = document.getElementById('tip')
+const totalField = document.getElementById('total')
+
+function calculateTotal() {
+  const billValue = billInput.value
+  const tipValue = tipInput.value
+  const totalValue = billValue * ((1 + tipValue) / 100)
+  totalField.innerContent = totalValue
+}
+
+btnEl.addEventListener('click', calculateTotal)
